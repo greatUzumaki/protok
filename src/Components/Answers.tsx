48494e39c33a906191b72139e022387de2474d6a
Alexey Pargachev <pargachev20@gmail.com>
2021-10-27 23:11:30 +0700

    add calc

--- src/Components/Answers.tsx
+++ src/Components/Answers.tsx
@@ -1,10 +1,148 @@
+import {
+  FormControl,
+  FormControlLabel,
+  InputAdornment,
+  MenuItem,
+  Radio,
+  RadioGroup,
+  Select,
+  TextField,
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
 import React from 'react';
+import { Iqa } from '../Configs/QA';
+
+const useStyles = makeStyles(() => ({
+  radio: {
+    color: '#ffffff',
+    fontWeight: 400,
+  },
+  radioContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  text: {
+    width: 450,
+    '& .MuiOutlinedInput-root': {
+      '&.Mui-focused fieldset': {
+        border: 1,
+      },
+    },
+    '@media (max-width: 800px)': {
+      width: 250,
+    },
+  },
+  number: {
+    width: 130,
+    '& .MuiOutlinedInput-root': {
+      '&.Mui-focused fieldset': {
+        border: 1,
+      },
+    },
+  },
+  select: {
+    width: 'max-content',
+    minWidth: 200,
+    '@media (max-width: 800px)': {
+      width: 300,
+    },
+  },
+}));
 
 interface IAnsw {
-  type: string;
-  answers: string[];
+  answ: Iqa;
+  id: number;
+  userValues: [];
 }
 
+const RadioAnsw = (props: IAnsw) => {
+  const classes = useStyles();
+  const [value, setValue] = React.useState('');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue((event.target as HTMLInputElement).value);
+  };
+
+  const handleChangeSelect = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setValue(event.target.value as string);
+  };
+
+  return props.answ.answers && props.answ.answers?.length > 4 ? (
+    <FormControl fullWidth>
+      <Select
+        value={value}
+        onChange={handleChangeSelect}
+        className={classes.select}
+        variant='outlined'
+      >
+        {props.answ.answers.map((answ) => {
+          return <MenuItem value={answ}>{answ}</MenuItem>;
+        })}
+      </Select>
+    </FormControl>
+  ) : (
+    <FormControl component='fieldset'>
+      <RadioGroup
+        name={props.answ.title}
+        className={classes.radioContainer}
+        value={value}
+        onChange={handleChange}
+      >
+        {props.answ.answers?.map((answ) => (
+          <FormControlLabel
+            className={classes.radio}
+            value={answ}
+            control={<Radio />}
+            label={answ}
+            labelPlacement='top'
+          />
+        ))}
+      </RadioGroup>
+    </FormControl>
+  );
+};
+
+const TextAnsw = (props: IAnsw) => {
+  const classes = useStyles();
+
+  return props.answ.type === 'number' ? (
+    <TextField
+      variant='outlined'
+      type='number'
+      InputProps={{
+        inputProps: {
+          min: props.answ.answers && props.answ.answers[0],
+          max: props.answ.answers && props.answ.answers[1],
+        },
+        endAdornment: (
+          <InputAdornment position='end'>{props.answ.label}</InputAdornment>
+        ),
+      }}
+      error={true}
+      className={classes.number}
+      defaultValue={props.answ.answers && props.answ.answers[0]}
+    />
+  ) : (
+    <TextField variant='outlined' className={classes.text} />
+  );
+};
+
 export const Answers = (props: IAnsw) => {
-  return <div></div>;
+  return (
+    <div>
+      {props.answ.type === 'radio' ? (
+        <RadioAnsw
+          answ={props.answ}
+          id={props.id}
+          userValues={props.userValues}
+        />
+      ) : (
+        <TextAnsw
+          answ={props.answ}
+          id={props.id}
+          userValues={props.userValues}
+        />
+      )}
+    </div>
+  );
 };
--- src/Components/Calc.tsx
+++ src/Components/Calc.tsx
@@ -22,6 +22,11 @@ const useStyles = makeStyles(() => ({
     letterSpacing: 1,
   },
   container: {
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    gap: 90,
     flexGrow: 1,
   },
   button: {
@@ -29,13 +34,18 @@ const useStyles = makeStyles(() => ({
     color: '#ffffff',
     border: 'solid 1px',
     borderRadius: 2,
+    letterSpacing: 1,
   },
   buttonContainer: {
     justifyContent: 'space-between',
     display: 'flex',
   },
   question: {
+    fontSize: 20,
     color: '#ffffff',
+    letterSpacing: 1,
+    fontWeight: 600,
+    borderBottom: 'solid 1px white',
   },
   titleContainer: {
     display: 'flex',
@@ -43,27 +53,61 @@ const useStyles = makeStyles(() => ({
     justifyContent: 'space-between',
     alignItems: 'center',
   },
+  root2: {
+    backgroundColor: secondColor,
+    height: 550,
+    width: 700,
+    boxShadow: '10px 10px 20px 0px rgb(37 42 46 / 20%);',
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    gap: 40,
+    padding: 40,
+  },
+  desc: {
+    fontWeight: 500,
+    color: '#ffffff',
+    fontSize: 18,
+    letterSpacing: 1,
+  },
 }));
 
 export const Calc = () => {
   const [currentQ, setCurrentQ] = useState(0);
+  const [finish, setFinish] = useState(false);
+
   const classes = useStyles();
 
-  return (
+  const maxQuestions = questionAnswer.length;
+
+  const userValues: any = [];
+
+  return finish ? (
+    <div className={classes.root2}>
+      <Typography className={classes.title} align='center'>
+        Спасибо за ответы!
+      </Typography>
+      <Typography align='center' className={classes.desc}>
+        Формируем проект, в ближайшее время с Вами свяжется наш менеджер
+      </Typography>
+    </div>
+  ) : (
     <div className={classes.root}>
       <Box className={classes.titleContainer}>
         <Typography className={classes.title}>Вопрос</Typography>
         <Typography className={classes.title}>
-          {currentQ + 1}/{questionAnswer.length}
+          {currentQ + 1}/{maxQuestions}
         </Typography>
       </Box>
       <Box className={classes.container}>
-        <Typography className={classes.question}>
+        <Typography className={classes.question} align='center'>
           {questionAnswer[currentQ].title}
         </Typography>
         <Answers
-          type={questionAnswer[currentQ].type}
-          answers={questionAnswer[currentQ].answers}
+          answ={questionAnswer[currentQ]}
+          id={currentQ}
+          userValues={userValues}
         />
       </Box>
       <Box className={classes.buttonContainer}>
@@ -79,13 +123,23 @@ export const Calc = () => {
           <div></div>
         )}
 
-        <Button
-          className={classes.button}
-          endIcon={<ArrowForward />}
-          onClick={() => setCurrentQ((old) => old + 1)}
-        >
-          Далее
-        </Button>
+        {currentQ + 1 === maxQuestions ? (
+          <Button
+            className={classes.button}
+            endIcon={<ArrowForward />}
+            onClick={() => setFinish(true)}
+          >
+            Завершить опрос
+          </Button>
+        ) : (
+          <Button
+            className={classes.button}
+            endIcon={<ArrowForward />}
+            onClick={() => setCurrentQ((old) => old + 1)}
+          >
+            Далее
+          </Button>
+        )}
       </Box>
     </div>
   );
